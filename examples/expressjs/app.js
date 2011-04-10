75176be083c714915bb70ce1d84779dbbf65e5b1
lexer <lexer@lexer-desktop.(none)>
2011-04-10 21:46:30 +0600

    update samples

--- examples/expressjs/app.js
+++ examples/expressjs/app.js
@@ -33,14 +33,32 @@ app.configure('production', function(){
 
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'Action boy demo'
   });
 });
 
-app.post('/foo', function(req, res) {
-  res.send({
-    success: true
+app.get('/form', function(req, res){
+  res.render('form', {
+    title: 'Form Sample'
   });
+});
+
+app.get('/actionlink', function(req, res){
+  res.render('actionlink', {
+    title: 'Action Link Sample'
+  });
+});
+
+app.post('/actionlink', function(req, res) {
+  console.log('action link called');
+  res.send({
+    foo: 'bar'
+  });
+});
+
+app.post('/form', function(req, res) {
+  console.log(req.body.user);
+  res.send({status: 'ok'});
 });
 
 // Only listen on $ node app.js
