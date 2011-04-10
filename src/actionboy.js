844e4571b552979a5dfc4240c89f960ef2629f74
lexer <lexer@lexer-desktop.(none)>
2011-04-10 21:05:03 +0600

    add form serialization

--- src/actionboy.js
+++ src/actionboy.js
@@ -1,11 +1,31 @@
 (function($) {
-  $.fn.invoke = function() { 
-    var $this = $(this),
-    data = $this.data();
+  $.fn.invoke = function(beforeSend) { 
+    var element = $(this),
+        data,
+        method,
+        url,
+        dataType = element.data('type') || ($.ajaxSettings && $.ajaxSettings.dataType);
+
+    if (element.is('form')) {
+      method = element.attr('method');
+      url = element.attr('action');
+      data = element.serializeArray();
+    } else if (element.is('a')) {
+      method = element.data('method');
+      url = element.attr('href');
+      data = null;
+    } else {
+      method = element.data('method');
+      url = element.data('action');
+      data = null;      
+    }
+
     return $.ajax({
-      type: data.method,
-      url: $this.attr('href'),
-      dataType: data.type
+      type: method,
+      beforeSend: beforeSend,
+      url: url,
+      data: data,
+      dataType: dataType
     }); 
   };
 })(jQuery);
